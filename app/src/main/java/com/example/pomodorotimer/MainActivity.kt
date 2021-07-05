3545333eef543e3b9ae92ae6d9562f8b90b7893a
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-07-06 00:00:39 +0900

    DESIGN | Design Main Screen UI

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val textViewRemainMinutes: TextView by lazy {
+        findViewById(R.id.textViewRemainMinutes)
+    }
+
+    private val textViewRemainSeconds: TextView by lazy {
+        findViewById(R.id.textViewRemainSeconds)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    textViewRemainMinutes.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+            }
+        )
     }
 }
